a9b31353eb27cf4cb312f1e1ecdb9ea0d72f305d
Verity <joshua-schmidt@online.de>
2021-02-19 15:03:15 +0100

    :construction: Add axios request to backend '/playlist'

--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 let button = document.getElementById("button")
 
 if (button != null) {
@@ -8,12 +10,23 @@ if (button != null) {
         if (textfield != null) {
             let value = textfield.value
 
-            if(validURL(value)) {
+            if (validURL(value)) {
                 console.log("Valid url")
                 let split = value.split("/")
-                let id = split[split.length-1].split("?")[0]
+                let id = split[split.length - 1].split("?")[0]
                 console.log(`ID: ${id}`)
-            }else {
+
+                axios.get("http://localhost:3030/playlist", {
+                    params: {
+                        id: id
+                    }
+                }).then(function (response) {
+                    console.log(response);
+                }).catch(function (error) {
+                    console.log(error);
+                }).then(function () {
+                })
+            } else {
                 console.log("Invalid url")
             }
 
