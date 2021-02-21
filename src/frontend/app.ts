841f075776c502275aded4eb093c4e0fb4e490c6
Verity <joshua-schmidt@online.de>
2021-02-21 18:59:52 +0100

    :sparkles: Add support for spotify uri of playlist

--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -8,45 +8,16 @@ if (button != null) {
 
         if (textField != null) {
             let value = textField.value
-
-            if (validURL(value)) {
+            if (matchesURL(value)) {
                 let split = value.split("/")
                 let id = split[split.length - 1].split("?")[0]
 
-                fetch(`http://localhost:3030/playlist?id=${id}`, {
-                    method: 'GET',
-                    mode: 'cors',
-                    headers: {
-                        'content-type': 'application/json'
-                    }
-                }).then(response => response.json()).then(data => {
-                    let body = data.body
-                    let mgkSongs = data.body.mgkSongs
-                    let otherSongs = data.body.otherSongs
+                call(textField, content, value, id)
+            }else if(matchesURI(value)) {
+                let split = value.split(":")
+                let id = split[split.length - 1]
 
-                    let textContent = `Rate: ${body.rate}%<br><br><u>Songs made by Machine Gun Kelly:</u>`
-
-                    for (let i = 0; i < mgkSongs.length; i++) {
-                        let curTrack = mgkSongs[i]
-                        let name = curTrack.name
-                        let artist = curTrack.artists.join(", ")
-
-                        textContent += `<br>${i + 1}. ${name} from ${artist}`
-                    }
-
-                    textContent+=`<br><br><u>Songs made by other artists:</u>`
-
-                    for (let i = 0; i < otherSongs.length; i++) {
-                        let curTrack = otherSongs[i]
-                        let name = curTrack.name
-                        let artist = curTrack.artists.join(", ")
-
-                        textContent += `<br>${i + 1}. ${name} from ${artist}`
-                    }
-
-                    content.innerHTML = textContent
-                })
-
+                call(textField, content, value, id)
             } else {
                 console.log("Invalid url")
             }
@@ -58,12 +29,49 @@ if (button != null) {
     })
 }
 
-function validURL(str) {
-    const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-    return pattern.test(str);
+function call(textField: HTMLInputElement, content: HTMLParagraphElement, value: String, id: String) {
+
+    fetch(`http://localhost:3030/playlist?id=${id}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json()).then(data => {
+        let body = data.body
+        let mgkSongs = data.body.mgkSongs
+        let otherSongs = data.body.otherSongs
+
+        let textContent = `Rate: ${body.rate}%<br><br><u>Songs made by Machine Gun Kelly:</u>`
+
+        for (let i = 0; i < mgkSongs.length; i++) {
+            let curTrack = mgkSongs[i]
+            let name = curTrack.name
+            let artist = curTrack.artists.join(", ")
+
+            textContent += `<br>${i + 1}. ${name} from ${artist}`
+        }
+
+        textContent += `<br><br><u>Songs made by other artists:</u>`
+
+        for (let i = 0; i < otherSongs.length; i++) {
+            let curTrack = otherSongs[i]
+            let name = curTrack.name
+            let artist = curTrack.artists.join(", ")
+
+            textContent += `<br>${i + 1}. ${name} from ${artist}`
+        }
+
+        content.innerHTML = textContent
+    })
+}
+
+function matchesURL(str) {
+    const pattern = RegExp('^(https:\\/\\/open.spotify.com\\/playlist\\/(.+))')
+    return pattern.test(str)
+}
+
+function matchesURI(str) {
+    const pattern = RegExp('^(spotify[\\/:]playlist(.+))')
+    return pattern.test(str)
 }
